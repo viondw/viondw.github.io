b401ae22eeeebf1e7ab475ed9cdffd61ef9f4968
ava anwan <ovo@uord.de>
2025-02-11 23:11:28 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,11 +30,11 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/viondw/viondw.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/viondw/viondw.github.io/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
